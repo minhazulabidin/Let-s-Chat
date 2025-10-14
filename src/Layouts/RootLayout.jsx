2fb198abe7e66a932346fb945951c2feed875060
minhazulabidin <minhajulabedin648@gmail.com>
2025-10-14 10:58:00 +0600

    navbar added

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react'
 import { Outlet } from 'react-router'
+import Navbar from '../Components/Navbar'
 
 const RootLayout = () => {
-    
+
     useEffect(() => {
 
     }, [])
 
     return (
         <>
+            <Navbar />
             <Outlet />
         </>
     )
