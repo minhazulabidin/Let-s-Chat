61a370edf9d36065f42d1075559e3ef0144e8a9f
introtoxicXXl <minhajulabedin648@gmail.com>
2025-10-29 20:19:09 +0600

    message perfectly sent and recive

--- src/Pages/Message.jsx
+++ src/Pages/Message.jsx
@@ -3,10 +3,11 @@ import Sidebar from '../Components/Message/Sidebar'
 import { useSelector } from 'react-redux'
 import img from "../assets/message.png"
 import { IoIosLock } from "react-icons/io";
-import { getDatabase, push, ref, set } from 'firebase/database';
+import { getDatabase, onValue, push, ref, set } from 'firebase/database';
 
 const Message = () => {
     const [msg, setMsg] = useState("")
+    const [msgList, setMsgList] = useState([])
     const selectUser = useSelector(state => state.MessageSlice.user)
     const user = useSelector(state => state.userSlice.user)
     const db = getDatabase()
@@ -15,6 +16,8 @@ const Message = () => {
         setMsg(e.target.value)
     }
 
+
+    // message sent
     const handleSentMsg = () => {
         set(push(ref(db, "msglist/")), {
             senderName: user.displayName,
@@ -31,9 +34,20 @@ const Message = () => {
         })
     }
 
+    useEffect(() => {
+        const msgRef = ref(db, "msglist/");
+        onValue(msgRef, (snapshot) => {
+            const arr = [];
+            snapshot.forEach(item => {
+                if (user.uid == item.val().senderId && selectUser.id == item.val().receiverId || user.uid == item.val().receiverId && selectUser.id == item.val().senderId) {
+                    arr.push(item.val())
+                }
+            })
+            setMsgList(arr)
+        })
+    }, [selectUser])
 
-
-
+    console.log(msgList)
     return (
         <>
             {/* component */}
@@ -54,34 +68,39 @@ const Message = () => {
                         </header>
                         {/* Chat Messages */}
                         <div className="h-screen overflow-y-auto p-4 pb-36">
-                            {/* Incoming Message */}
-                            <div className="flex mb-4 cursor-pointer">
-                                <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
-                                    <img
-                                        src="https://placehold.co/200x/ffa8e4/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
-                                        alt="User Avatar"
-                                        className="w-8 h-8 rounded-full"
-                                    />
-                                </div>
-                                <div className="flex max-w-96 bg-white rounded-lg p-3 gap-3">
-                                    <p className="text-gray-700">Hey Bob, how's it going?</p>
-                                </div>
-                            </div>
-                            {/* Outgoing Message */}
-                            <div className="flex justify-end mb-4 cursor-pointer">
-                                <div className="flex max-w-96 bg-indigo-500 text-white rounded-lg p-3 gap-3">
-                                    <p>
-                                        Hi Alice! I'm good, just finished a great book. How about you?
-                                    </p>
-                                </div>
-                                <div className="w-9 h-9 rounded-full flex items-center justify-center ml-2">
-                                    <img
-                                        src="https://placehold.co/200x/b7a8ff/ffffff.svg?text=ʕ•́ᴥ•̀ʔ&font=Lato"
-                                        alt="My Avatar"
-                                        className="w-8 h-8 rounded-full"
-                                    />
-                                </div>
-                            </div>
+                            {
+                                msgList.map(item => (
+                                    item.senderId == user.uid
+                                        ?
+                                        <div className="flex justify-end mb-4 cursor-pointer">
+                                            <div className="flex max-w-96 bg-indigo-500 text-white rounded-lg p-3 gap-3">
+                                                <p>
+                                                    {item.msg}
+                                                </p>
+                                            </div>
+                                            <div className="w-9 h-9 rounded-full flex items-center justify-center ml-2">
+                                                <img
+                                                    src={item.photo}
+                                                    alt="My Avatar"
+                                                    className="w-8 h-8 rounded-full"
+                                                />
+                                            </div>
+                                        </div>
+                                        :
+                                        <div className="flex mb-4 cursor-pointer">
+                                            <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
+                                                <img
+                                                    src={item.photo}
+                                                    alt="User Avatar"
+                                                    className="w-8 h-8 rounded-full"
+                                                />
+                                            </div>
+                                            <div className="flex max-w-96 bg-white rounded-lg p-3 gap-3">
+                                                <p className="text-gray-700">{item.msg}</p>
+                                            </div>
+                                        </div>
+                                ))
+                            }
                         </div>
                         {/* Chat Input */}
                         <footer className="bg-white border-t border-gray-300 p-4 absolute bottom-16 w-3/4">
